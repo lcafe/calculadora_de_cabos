1a40ec44e19a490e190b5f5c5c0f05cbf8efbd07
Lucas Ferreira <182416116+lcafe@users.noreply.github.com>
2026-01-04 02:53:26 -0300

    adicionado cálculo da corrente de projeto total e por cabo, sistemas mono/bifasicos e trifasicos.

---
+++ pkg/calculations/low_voltage/NominalCurrentPerCable.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+	"fmt"
+	"math"
+)
+
+func NominalCurrentPerCable(
+	nominalCurrent float64,
+	cablePerPhase int,
+) (float64, error) {
+
+	if math.IsNaN(nominalCurrent) || math.IsInf(nominalCurrent, 0) {
+		return 0, fmt.Errorf("Corrente nominal inválida: valor não numérico ou infinito")
+	}
+
+	if cablePerPhase <= 0 {
+		return 0, fmt.Errorf("Quantidade de cabos por fase inválida: deve ser maior que zero")
+	}
+
+	result := nominalCurrent / float64(cablePerPhase)
+
+	return result, nil
+}
--- pkg/calculations/low_voltage/NominalCurrentThreePhase.go
+++ pkg/calculations/low_voltage/NominalCurrentThreePhase.go
@@ -5,12 +5,16 @@ import (
 	"math"
 )
 
-func NominalCurrent(power float64, voltage float64, powerFactor float64, demandFactor float64) (float64, error) {
+func NominalCurrentThreePhase(
+	power float64,
+	voltage float64,
+	powerFactor float64,
+	demandFactor float64,
+) (float64, error) {
 
 	if math.IsNaN(power) || math.IsInf(power, 0) {
 		return 0, fmt.Errorf("Potência inválida: valor não numérico ou infinito")
 	}
-
 	if power <= 0 {
 		return 0, fmt.Errorf(
 			"Potência inválida: deve ser maior que zero (valor informado: %.2f)",
@@ -21,7 +25,6 @@ func NominalCurrent(power float64, voltage float64, powerFactor float64, demandF
 	if math.IsNaN(voltage) || math.IsInf(voltage, 0) {
 		return 0, fmt.Errorf("Tensão inválida: valor não numérico ou infinito")
 	}
-
 	if voltage <= 0 {
 		return 0, fmt.Errorf(
 			"Tensão inválida: deve ser maior que zero (valor informado: %.2f)",
@@ -30,42 +33,39 @@ func NominalCurrent(power float64, voltage float64, powerFactor float64, demandF
 	}
 
 	if math.IsNaN(powerFactor) || math.IsInf(powerFactor, 0) {
-		return 0, fmt.Errorf("Fator de potência inválida: valor não numérico ou infinito")
+		return 0, fmt.Errorf("Fator de potência inválido: valor não numérico ou infinito")
 	}
-
 	if powerFactor <= 0 {
 		return 0, fmt.Errorf(
-			"Fator de potência inválida: deve ser maior que zero (valor informado: %.2f)",
+			"Fator de potência inválido: deve ser maior que zero (valor informado: %.2f)",
 			powerFactor,
 		)
 	}
-
 	if powerFactor > 1 {
 		return 0, fmt.Errorf(
-			"Fator de potência inválida: deve ser menor que um (valor informado: %.2f)",
+			"Fator de potência inválido: deve ser menor ou igual a 1 (valor informado: %.2f)",
 			powerFactor,
 		)
 	}
 
 	if math.IsNaN(demandFactor) || math.IsInf(demandFactor, 0) {
-		return 0, fmt.Errorf("Fator de demanda inválida: valor não numérico ou infinito")
+		return 0, fmt.Errorf("Fator de demanda inválido: valor não numérico ou infinito")
 	}
-
 	if demandFactor <= 0 {
 		return 0, fmt.Errorf(
 			"Fator de demanda inválido: deve ser maior que zero (valor informado: %.2f)",
 			demandFactor,
 		)
 	}
-
 	if demandFactor > 1 {
 		return 0, fmt.Errorf(
-			"Fator de demanda inválido: deve ser menor que um (valor informado: %.2f)",
+			"Fator de demanda inválido: deve ser menor ou igual a 1 (valor informado: %.2f)",
 			demandFactor,
 		)
 	}
 
-	result := ((power * demandFactor) / (voltage * powerFactor))
+	result := (power * demandFactor) /
+		(math.Sqrt(3) * voltage * powerFactor)
 
 	return result, nil
 }
